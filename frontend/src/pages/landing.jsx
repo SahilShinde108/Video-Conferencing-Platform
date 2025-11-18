02a43487cae042d13955b3acd98ce271e88f2c8f
Sahil Shinde <sahilshinde9229@gmail.com>
2025-11-19 00:38:20 +0530

    Project Files Added

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import '../App.css'
+import { Link } from 'react-router-dom'
 
 export default function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div className='landingPageContainer'>
+      <nav>
+        <div className='navHeader'>
+          <h2>Apna Video Call</h2>
+        </div>
+        <div className='navList'>
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <div role='button'>
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+
+      <div className="landingMainContainer">
+        <div>
+          <h1><span style={{color: "#FF9839"}}>Connect</span> with your loved ones</h1>
+
+          <p>Cover a distance with Apna Video Call</p>
+          <div role='button'>
+            <Link to={"/home"}>Get Started</Link>
+          </div>
+        </div>
+        <div>
+          <img src='/mobile.png' alt=''></img>
+        </div>
+      </div>
+    </div>
   )
 }
